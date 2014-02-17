c86da351d0eddbf13833a365595ab952582f6cac
Zhonglei Qiu <zhongleiqiu@gmail.com>
2014-02-17 10:51:10 +0800

    add test

---
+++ test/browerstack-webdriver.js
@@ -0,0 +1,25 @@
+// https://www.browserstack.com/automate/node
+
+var webdriver = require('browserstack-webdriver');
+
+// Input capabilities
+var capabilities = {
+	'browserName' : 'firefox',
+	'browserstack.user' : 'zhongleiqiu',
+	'browserstack.key' : 'GDB89zAsZyygLWtdoYMd'
+}
+
+var driver = new webdriver.Builder().
+	usingServer('http://hub.browserstack.com/wd/hub').
+	withCapabilities(capabilities).
+	build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack');
+driver.findElement(webdriver.By.name('btnG')).click();
+
+driver.getTitle().then(function(title) {
+	console.log(title);
+});
+
+driver.quit();
