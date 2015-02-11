fba3b8d56984567e6b6f9a240679c9b729072e35
Zhonglei Qiu <zhongleiqiu@gmail.com>
2015-02-11 17:52:16 +0800

    update testswarm

--- test/browerstack-webdriver.js
+++ test/browerstack-webdriver.js
@@ -3,16 +3,25 @@
 var webdriver = require('browserstack-webdriver'),
 
 	// Input capabilities
+  // 所有支持的浏览器和系统的配置： https://www.browserstack.com/list-of-browsers-and-platforms?product=automate
 	capabilities = {
 		'browserName' : 'firefox',
 		'browserstack.user' : 'zhongleiqiu',
 		'browserstack.key' : 'GDB89zAsZyygLWtdoYMd'
 	},
 
+  // run on BrowserStack
+  browserStackServer = 'http://hub.browserstack.com/wd/hub',
+
+  // Running selenium-server.jar at port 4444
+  //localServer = 'http://localhost:4444',
+
 	driver = new webdriver.Builder().
-		usingServer('http://hub.browserstack.com/wd/hub').
+		usingServer(browserStackServer).
 		withCapabilities(capabilities).
 		build();
+
+
 
 driver.get('http://www.google.com/ncr');
 driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack');
